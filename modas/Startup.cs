b51e7a98f95e5e9e8571aa82144390fcff757781
smarshall5 <smarshall5@my.wctc.edu>
2020-01-27 14:15:28 -0600

    Modified data for database seeding

--- modas/Startup.cs
+++ modas/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Modas.Models;
 
-namespace modas
+namespace Modas
 {
     public class Startup
     {
@@ -15,6 +11,8 @@ namespace modas
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEventRepository, FakeEventRepository>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +23,8 @@ namespace modas
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvcWithDefaultRoute();
+            app.UseStaticFiles();
         }
     }
 }
